02283d0c817356241aa286c07585ea1cf2706484
Brian Di Palma <offler@gmail.com>
2014-01-19 19:19:34 +0000

    Create directory structure and static files for ES6 custom element package.

--- bin/create-customelement-package-cli.js
+++ bin/create-customelement-package-cli.js
@@ -1,6 +1,35 @@
 #!/usr/local/bin/node
-
 "use strict";
 
-console.info("Hello from a globally installed package!");
+var fs = require("fs"),
+	path = require("path");
 
+if(process.argv.length < 3) {
+	console.error("Usage: create-customelement-package <customelement-package-name>");
+
+	process.exit(1);
+}
+
+var packageName = process.argv[2],
+	packageDirectory = packageName + path.sep,
+	packageSrcDirectory = packageDirectory + "src",
+	packageIndexFile = packageDirectory + "index.html",
+	packageServerFile = packageDirectory + "server.js",
+	templateDirectory = ".." + path.sep + "template" + path.sep,
+	templateIndexFile = templateDirectory + "index.js",
+	templateServerFile = templateDirectory + "server.js";
+
+console.info("Creating the custom element package,", packageName);
+
+fs.mkdirSync(packageName);
+fs.mkdirSync(packageSrcDirectory);
+
+var templateIndexFileReadStream = fs.createReadStream(templateIndexFile),
+	packageIndexFileWriteStream = fs.createWriteStream(packageIndexFile),
+	templateServerFileReadStream = fs.createReadStream(templateServerFile),
+	packageServerFileWriteStream = fs.createWriteStream(packageServerFile);
+
+templateIndexFileReadStream.pipe(packageIndexFileWriteStream);
+templateServerFileReadStream.pipe(packageServerFileWriteStream);
+
+//Needed files: package.json
