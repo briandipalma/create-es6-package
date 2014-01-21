37c5990f768f23bc7942d4bc6ec45527bb7de1a4
Brian Di Palma <offler@gmail.com>
2014-01-21 08:56:50 +0000

    Adding ability to install from npm programatically to generator.

--- bin/create-customelement-package-cli.js
+++ bin/create-customelement-package-cli.js
@@ -2,6 +2,7 @@
 "use strict";
 
 var fs = require("fs"),
+	npm = require("npm"),
 	path = require("path");
 
 if(process.argv.length < 3) {
@@ -15,9 +16,11 @@ var packageName = process.argv[2],
 	packageSrcDirectory = packageDirectory + "src",
 	packageIndexFile = packageDirectory + "index.html",
 	packageServerFile = packageDirectory + "server.js",
+	packagePackageJsonFile = packageDirectory + "package.json",
 	templateDirectory = ".." + path.sep + "template" + path.sep,
-	templateIndexFile = templateDirectory + "index.js",
-	templateServerFile = templateDirectory + "server.js";
+	templateIndexFile = templateDirectory + "index.html",
+	templateServerFile = templateDirectory + "server.js",
+	templatePackageJsonFile = templateDirectory + "package.json";
 
 console.info("Creating the custom element package,", packageName);
 
@@ -27,9 +30,26 @@ fs.mkdirSync(packageSrcDirectory);
 var templateIndexFileReadStream = fs.createReadStream(templateIndexFile),
 	packageIndexFileWriteStream = fs.createWriteStream(packageIndexFile),
 	templateServerFileReadStream = fs.createReadStream(templateServerFile),
-	packageServerFileWriteStream = fs.createWriteStream(packageServerFile);
+	packageServerFileWriteStream = fs.createWriteStream(packageServerFile),
+	templatePackageJsonFileReadStream = fs.createReadStream(templatePackageJsonFile),
+	packagePackageJsonFileWriteStream = fs.createWriteStream(packagePackageJsonFile);
 
 templateIndexFileReadStream.pipe(packageIndexFileWriteStream);
 templateServerFileReadStream.pipe(packageServerFileWriteStream);
+templatePackageJsonFileReadStream.pipe(packagePackageJsonFileWriteStream);
 
-//Needed files: package.json
+npm.load(function(err, npm) {
+	npm.config.set("save", true);
+
+	npm.commands.install(packageName, ["es6-module-loader", "traceur"], function() {
+		npm.config.set("save-dev", true);
+		
+		npm.commands.install(packageName, ["node-static"], function() {
+			npm.prefix = process.cwd() + path.sep + packageName;
+			
+			console.log("prefix = %s", npm.prefix);
+			
+			npm.commands.dedupe();
+		});
+	});
+});
