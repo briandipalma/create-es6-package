e42e48a0ea24d20ec793e8d82d258bc6c08dc1b8
Brian Di Palma <offler@gmail.com>
2014-01-22 08:59:37 +0000

    Refactor of npm loading logic and removing npm dedupe as it doesn't seem to work OK.

--- bin/create-customelement-package-cli.js
+++ bin/create-customelement-package-cli.js
@@ -14,42 +14,36 @@ if(process.argv.length < 3) {
 var packageName = process.argv[2],
 	packageDirectory = packageName + path.sep,
 	packageSrcDirectory = packageDirectory + "src",
-	packageIndexFile = packageDirectory + "index.html",
-	packageServerFile = packageDirectory + "server.js",
-	packagePackageJsonFile = packageDirectory + "package.json",
-	templateDirectory = ".." + path.sep + "template" + path.sep,
-	templateIndexFile = templateDirectory + "index.html",
-	templateServerFile = templateDirectory + "server.js",
-	templatePackageJsonFile = templateDirectory + "package.json";
+	templateDirectory = ".." + path.sep + "template" + path.sep;
 
 console.info("Creating the custom element package,", packageName);
 
 fs.mkdirSync(packageName);
 fs.mkdirSync(packageSrcDirectory);
 
-var templateIndexFileReadStream = fs.createReadStream(templateIndexFile),
-	packageIndexFileWriteStream = fs.createWriteStream(packageIndexFile),
-	templateServerFileReadStream = fs.createReadStream(templateServerFile),
-	packageServerFileWriteStream = fs.createWriteStream(packageServerFile),
-	templatePackageJsonFileReadStream = fs.createReadStream(templatePackageJsonFile),
-	packagePackageJsonFileWriteStream = fs.createWriteStream(packagePackageJsonFile);
+copyFileFromTemplateToNewlyCreatedPackage("server.js");
+copyFileFromTemplateToNewlyCreatedPackage("index.html");
+copyFileFromTemplateToNewlyCreatedPackage("package.json");
 
-templateIndexFileReadStream.pipe(packageIndexFileWriteStream);
-templateServerFileReadStream.pipe(packageServerFileWriteStream);
-templatePackageJsonFileReadStream.pipe(packagePackageJsonFileWriteStream);
+npm.load(npmLoaded);
 
-npm.load(function(err, npm) {
+function copyFileFromTemplateToNewlyCreatedPackage(fileName) {
+	var packageFile = packageDirectory + fileName,
+		templateFile = templateDirectory + fileName,
+		templateFileReadStream = fs.createReadStream(templateFile),
+		packageFileWriteStream = fs.createWriteStream(packageFile);
+	
+	templateFileReadStream.pipe(packageFileWriteStream);
+};
+
+function npmLoaded(err, npm) {
 	npm.config.set("save", true);
 
-	npm.commands.install(packageName, ["es6-module-loader", "traceur"], function() {
-		npm.config.set("save-dev", true);
-		
-		npm.commands.install(packageName, ["node-static"], function() {
-			npm.prefix = process.cwd() + path.sep + packageName;
-			
-			console.log("prefix = %s", npm.prefix);
-			
-			npm.commands.dedupe();
-		});
-	});
-});
+	npm.commands.install(packageName, ["es6-module-loader", "traceur"], dependenciesInstalled);
+};
+
+function dependenciesInstalled() {
+	npm.config.set("save-dev", true);
+	
+	npm.commands.install(packageName, ["node-static"]);
+};
